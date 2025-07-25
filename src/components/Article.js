e5ce6c657a4499102a6ab2a9f96c40691bb1adc4
Jackson <jacksonmungai001@gmail.com>
2025-07-25 13:40:15 +0300

    new update

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  let indicator = "";
+  if (minutes < 30) {
+    const cups = Math.ceil(minutes / 5);
+    indicator = "☕️".repeat(cups) + ` ${minutes} min read`;
+  } else {
+    const bentos = Math.ceil(minutes / 10);
+    indicator = "🍱".repeat(bentos) + ` ${minutes} min read`;
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+      {minutes && <p>{indicator}</p>}
+    </article>
+  );
+}
+
+export default Article;
